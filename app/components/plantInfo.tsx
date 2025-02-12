4bb6426fd5bfa46026d0d126a9a89c2dd4a60c3e
Jacob Lindström <jacob.lindstrom@omegapoint.se>
2025-02-12 11:48:58 +0100

    last minute changes

--- app/components/plantInfo.tsx
+++ app/components/plantInfo.tsx
@@ -5,12 +5,17 @@ import { useEffect, useState } from "react";
 
 const PlantInfo = (plantData: any) => {
     const plantAuhtor = plantData.plantData.data.author
+    const familyName = plantData.plantData.data.family_common_name
+    const observations = plantData.plantData.data.observations
     console.log(plantAuhtor);
     
     return (
         <div>
             {plantData ? (
-                <pre>{plantAuhtor}</pre>
+                <pre>{plantAuhtor}---
+                {familyName}---{observations}
+                </pre>
+
             ) : (
                 <p>Loading...</p>
             )}
